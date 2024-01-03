46e4efc6cb206a83512105788bceedc1a14e06f9
“Kaloyan <kokoeverest@gmail.com>
2024-01-03 11:41:27 +0200

    first commit

---
+++ models/tax.py
@@ -0,0 +1,8 @@
+
+
+class Tax:
+    city: str | None
+    municipality: str | None
+    car_age: int | None
+    euro_category: int | None
+    car_power_kw: int | None
---
+++ scrapers/tax.py
@@ -0,0 +1,82 @@
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
+import requests
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+
+def wait_for_a_second(sec: int):
+    '''Wait a sec, don't put too much pressure on the server :-)'''
+    return time.sleep(sec)
+
+
+def extract_option_or_input_fields(elem, value, field_name="option"):
+    options = elem.find_elements(By.TAG_NAME, field_name)
+    result = []
+    for option in options:
+        result.append(option.text)
+        opt_value = option.get_attribute("value")
+        if opt_value == value and field_name == "option":
+            option.click()
+    return set(result)
+
+car_data = {
+    'obl': 'София', 
+    'obs': 'Столична', # def municipality_selector
+    'old': '1', # def age_convertor
+    'euro': '7', # def euro_category_converter
+    'kw': '74' # def hp_to_kw_converter
+}
+
+driver = webdriver.Chrome()
+
+# the url of the Car taxes calculator for the territory of Bulgaria
+url = "https://cartax.uslugi.io/"
+
+# just an artificial pause
+wait_for_a_second(1)
+
+# load the page contents with the Selenium webdriver 
+# (the site uses JS scripts to handle the form submission)
+driver.get(url)
+
+# locate the input fields of the form 
+# for this website there are 5 input fields, as defined in the car_data dict
+all_options = {}
+for k, v in car_data.items():
+    try:
+        el = driver.find_element(By.XPATH, f"//select[@name='{k}']")
+        all_options[k] = extract_option_or_input_fields(el, v)
+    except Exception as e:
+            # the 'kw' input field
+        try:
+            el = driver.find_element(By.XPATH, f"//input[@name='{k}']")
+            all_options[k] = extract_option_or_input_fields(el, v, field_name="input")
+            el.clear()
+            el.send_keys(v)
+            el.submit()
+        except Exception as e:
+            continue
+result = driver.find_element(By.CLASS_NAME, "amount").text
+print(result) # def get_price_as_float
+x=0
+
+# post_response = requests.post(url, data=car_data)
+# print(post_response.status_code)
+# post_page = post_response.text
+# post_soup = bs(post_page, 'html.parser')
+# search = post_soup.find(class_='amount')
+
+# print(search.text)
+
+# response = requests.get(url)
+# page = response.text
+# soup = bs(page, 'html.parser')
+# print(response.text)
+# links = soup.find('form')
+
+# for link in links:
+#     print(link)
+
